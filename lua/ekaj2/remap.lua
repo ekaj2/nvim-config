b15783726bd1a95e2c20f533373ad99b3731486e
Jake Duth <jakeduth2@gmail.com>
2023-08-19 23:11:19 -0400

    Add todo

--- lua/ekaj2/remap.lua
+++ lua/ekaj2/remap.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap("n", "<leader>fe", ":Ex<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>n", ":e ~/notes<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>t", ":e ~/todo<CR>", { noremap = true, silent = true })
