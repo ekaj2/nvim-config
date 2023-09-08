ebb8416c343ced196778dd2ea6224e9116425b5f
Jake Duth <jakeduth2@gmail.com>
2023-09-07 22:34:58 -0400

    Add a way to string search with a specific file extension

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -47,3 +47,24 @@ vim.keymap.set('n', '<leader>st', function()
     });
 end)
 
+-- Taken from: https://gitlab.com/sultanahamer/dotfiles/-/blob/master/nvim/lua/telescope_setup/init.lua
+-- From this answer: https://www.reddit.com/r/neovim/comments/tpnt3c/comment/i2chddu/
+-- Potentially better solution for down the road: https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/lua/tj/telescope/custom/multi_rg.lua
+local live_grep_in_glob = function(glob_pattern)
+  require('telescope.builtin').live_grep({
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--glob=" .. (glob_pattern or ""),
+    }
+  })
+end
+
+vim.keymap.set('n', '<leader>se', function()
+  vim.ui.input({ prompt = "File Glob: ", completion = "file", default = "**/*." }, live_grep_in_glob)
+end)
