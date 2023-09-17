0a4323569a7e23ec7b32a436053c1f5a883f1be4
Jake Duth <jakeduth2@gmail.com>
2023-09-17 13:32:09 -0400

    That was crazy, but got djlint setup
    https://github.com/sbdchd/neoformat
    https://djlint.com/docs/formatter/
    https://www.djlint.com/docs/getting-started/

--- after/plugin/neoformat.lua
+++ after/plugin/neoformat.lua
@@ -1,3 +1,20 @@
+-- Custom formatter for Django HTML
+-- TODO: detect if in a Django project to split htmldjango from html
+vim.g.neoformat_html_djlint = {
+	exe = "djlint",
+	args = {
+		"--reformat",
+		"--format-css",
+		"--format-js",
+		"--indent=2",
+	},
+	valid_exit_codes = { 0, 1, 130 },
+	stdin = false,
+	replace = true,
+}
+vim.g.neoformat_enabled_html = { "djlint" }
+vim.g.neoformat_verbose = 1
+
 vim.api.nvim_exec(
 	[[
   augroup fmt
